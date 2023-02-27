d86fb663cf0038ff69821e7d192789c6ef9aa466
Michael Haswell <Michael.Haswell@gov.bc.ca>
2023-02-27 14:05:43 -0800

    Changed active-events and resource-list blocks' category to "theme" as "emergencyinfo" custom category was not appearing in Editor for Pages or Posts (only CPTs)

--- checklist.md
+++ checklist.md
@@ -1,4 +1,4 @@
-Created at 2023-02-24 3:06 pm
+Created at 2023-02-27 2:04 pm
 
 * [yes] Updated version in composer.json
 * [yes] Updated version in style.css or plugin file
--- dist/Bcgov/EmergencyInfo/blocks/resource-list/index.asset.php
+++ dist/Bcgov/EmergencyInfo/blocks/resource-list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bb9082ce35da6adb59bc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e1644690d02f2a9f9e26');
