ab2134cd2f382ca848686fc26a39673c0bfe7000
Michael Haswell <Michael.Haswell@gov.bc.ca>
2023-05-05 10:38:04 -0700

    Added Wildfire event pattern export Changed event header to reusable block, added export Fixed margin/padding issue on small screen sizes Small styling fix on social media share block to better match prototypes

--- dist/scripts/admin.asset.php
+++ dist/scripts/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '80e4e61ba3835745c800');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8069bf516ff27a33770f');
