d7d09c4f1d7c0847c217bbcd0ae1081632ff9a81
Michael Haswell <Michael.Haswell@gov.bc.ca>
2023-05-19 11:41:36 -0700

    Added is_aggregated_event meta field to Evacuation field group Changed Event query loop to order by metadata date/time rather than default post date Added style to allow for text (numbers) to be used in places where an icon would normally be used Fixed spacing of button label in Post Social Share block

--- dist/scripts/admin.asset.php
+++ dist/scripts/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'aee658555f6c25ee1285');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '0743b58080e7b05781b3');
